0ad9b75efa4af89b99a9ddeb3335418be37c61ee
sofia-alvarezl <123980268+sofia-alvarezl@users.noreply.github.com>
2023-02-10 13:59:28 -0500

    resumen

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {});
+const cardAdj = [
+  {
+    name: "c",
+    img: "imagenes/c1.jpg"
+  }
+];
+
+const cuadricula = document.querySelector(".cuadricula");
+const resultado = document.querySelector("#resultado");
+var cartasescogidas = [];
+var cartasescogidassId = [];
+var cartasganadas = [];

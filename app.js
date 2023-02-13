b29be9da60e80aa15833b872f1a0da653b7ed885
julianitapp <123569438+julianitapp@users.noreply.github.com>
2023-02-13 12:32:38 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {});
-const cardAdj = [
-  {
-    name: "c",
-    img: "imagenes/c1.jpg"
-  }
-];
+//----------------- lecture_01 ----------------------------------//
+document.addEventListener("DOMContentLoaded", () => {
+  //----------------- lecture_02 ----------------------------------//
+  const cardAdj = [
+    { name: "c1", img: "images/c1.jpg" },
+    { name: "c2", img: "images/c2.jpg" },
+    { name: "c3", img: "images/c3.jpg" },
+    { name: "c4", img: "images/c4.jpg" },
+    { name: "c5", img: "images/c5.jpg" },
+    { name: "c6", img: "images/c6.jpg" },
+    { name: "c1", img: "images/c1.jpg" },
+    { name: "c2", img: "images/c2.jpg" },
+    { name: "c3", img: "images/c3.jpg" },
+    { name: "c4", img: "images/c4.jpg" },
+    { name: "c5", img: "images/c5.jpg" },
+    { name: "c6", img: "images/c6.jpg" }
+  ];
 
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasescogidas = [];
-var cartasescogidassId = [];
-var cartasganadas = [];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
+      // cuando i=cardAdj.length, es decir, 12.
+      var carta = document.createElement("img"); // Crea elemento img cada vez que se ejecuta la función.
+      carta.setAttribute("src", "images/reverso.png"); //Asignar nuevo atributo a cada carta
+      //igual a la imagen 'reverso'.
+      carta.setAttribute("data-id", i); //Asignar como atributo a cada carta creada, el id=i.
+      carta.addEventListener("click", voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta
+      //la función voltearCarta.
+      cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
+      // cuadricula de html para que se imprima la carta creada en el div de html.
+    }
+  }
+});

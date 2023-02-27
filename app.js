c7369b9a061bcd4c67825b8076cfa39f02349d96
sofia-alvarezl <123980268+sofia-alvarezl@users.noreply.github.com>
2023-02-27 12:42:05 -0500

    ardedfgq4

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {});
-const cardAdj = [
-  {
-    name: "c",
-    img: "imagenes/c1.jpg"
-  }
-];
+document.addEventListener("DOMContentLoaded", () => {
+  //----------------- lecture_02 ----------------------------------//
+  const cardAdj = [
+    { name: "c", img: "images/c1.jpg" },
+    { name: "css", img: "images/c2.jpg" },
+    { name: "html", img: "images/c3.jpg" },
+    { name: "c", img: "images/c4.jpg" },
+    { name: "css", img: "images/c5.jpg" },
+    { name: "html", img: "images/c6.jpeg" }
+  ];
 
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasescogidas = [];
-var cartasescogidassId = [];
-var cartasganadas = [];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+
+      carta.setAttribute("src", "images/c10.png");
+
+      carta.setAttribute("data-id", i);
+
+      carta.addEventListener("click", voltearCarta);
+
+      cuadricula.appendChild(carta);
+    }
+  }
+});
